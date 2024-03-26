4ed3110c0f67879af50df69a5cec7a4c16f6ea54
taoluan.dev <tvanluanst@gmail.com>
2024-03-27 00:55:35 +0700

    feat: add migration table store and products

---
+++ database/migrations/2024_03_26_175352_create_stores_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->integer('uuid')->unsigned();
+            $table->string('name');
+            $table->string('nameRoot')->nullable();
+            $table->timestamps();
+
+            // Add unique constraint on nameRoot column
+            $table->unique('nameRoot', 'nameRoot_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
